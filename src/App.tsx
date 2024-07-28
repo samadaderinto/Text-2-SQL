f3f8bbcc3b2db98535ca439c2499482e22914931
hassan <hassanajibade17@gmail.com>
2024-07-28 23:55:09 +0100

    signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import './App.css'
+// import './App.css'
 import Dashboard from './Components/Dashboard'
+import Signup from './Components/Signup'
+import './SCSS/Signup.scss'
 
 function App() {
 
   return (
     <>
+    <Signup/>
      <Dashboard/>
     </>
   )
---
+++ src/Components/Signup.tsx
@@ -0,0 +1,20 @@
+
+const Signup = () => {
+  return (
+    <div className="Signup_Container">
+        <section className="White_Section">
+            <span></span>
+            <h1>Welcome Back, To EchoCart</h1>
+            <article>New User? <p>Create an account</p></article>
+            <label htmlFor="email">Email</label>
+            <input type="text" placeholder="input your registered email" />
+            <label htmlFor="email">password</label>
+            <input type="text" placeholder="input your password" />
+
+        </section>
+        <section className="Blue_Section"></section>
+    </div>
+  )
+}
+
+export default Signup

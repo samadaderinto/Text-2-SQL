df28363b0f7c3aec08e2a2b43fe88690b223a2ec
ADEX-1 <adesamad1234@gmail.com>
2024-07-31 01:59:48 +0100

    added 404

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
-// import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
 import Dashboard from './Components/Dashboard'
 import SignIn from './Components/SignIn'
 import Signup from './Components/Signup'
 import Forgotpassword from './Components/Forgotpassword'
 import Newpassword from './Components/Newpassword'
+import PageNotFound from './Components/PageNotFound'
+
+
 import './SCSS/Main.scss'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
@@ -16,12 +20,14 @@ function App() {
      
      <Routes>
       <Route path='/' element={<Dashboard/>}/>
-
       <Route path='auth/signup' element={<Signup/>}/>
       <Route path='auth/signin' element={<SignIn/> }/>
       <Route path='auth/forgot-password' element={<Forgotpassword/>}/>
       <Route path='auth/new-password' element={<Newpassword/>}/>
+      <Route path="*" element={<PageNotFound/>}></Route>
      </Routes>
+
+     
      </BrowserRouter>
     </>
   )

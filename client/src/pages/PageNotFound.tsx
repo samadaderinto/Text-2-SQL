3d29ec2c498cde7f48a290a13e8e22def586b106
Hassan <hassanajibade17@gmail.com>
2024-08-16 10:40:07 +0100

    updated 404

--- client/src/pages/PageNotFound.tsx
+++ client/src/pages/PageNotFound.tsx
@@ -1,7 +1,17 @@
+import { Header } from "../layouts/Header";
+import SideBar from "../layouts/SideBar";
+
 export const PageNotFound = () => {
   return (
     <div className="Not_Found_Container">
-      page not found
+      <Header/>
+      <SideBar/>
+
+      <div className="Content_container">
+        <h1>Oops! Page Not Found</h1>
+        <p>It looks like the page you are looking for doesn't exist or has moved</p>
+        <p>Don't worry, you can always go back to your dashboard or try searching what you need!</p>
+      </div>
     </div>
   )
 }

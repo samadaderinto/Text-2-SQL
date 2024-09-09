da2d6079273347b02511a10b18882ffbb3c97a7d
ADEX-1 <adesamad1234@gmail.com>
2024-09-09 22:39:09 +0100

    .

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,44 +1,33 @@
-import axios from 'axios';
+import axios from "axios";
 
+const API_BASE_URL = "http://localhost:8000";
 
-const API_BASE_URL = 'http://localhost:8000';
+const api = axios.create({baseURL: API_BASE_URL});
 
-
-const api = axios.create({
-  baseURL: API_BASE_URL,
-});
-
-
-api.interceptors.request.use(
-  (config) => {
-    const accessToken = localStorage.getItem('accessToken');
-    if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      try {
-        const refreshToken = localStorage.getItem('refreshToken');
-        const response = await axios.post(`${API_BASE_URL}/refresh-token/`, { refreshToken });
-        const { accessToken } = response.data;
-        localStorage.setItem('accessToken', accessToken);
-        api.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-        return api(originalRequest);
-      } catch (refreshError) {
-        console.error('Token refresh failed:', refreshError);
-      }
-    }
-    return Promise.reject(error);
+api.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem("access");
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
   }
-);
+  return config;
+}, (error) => Promise.reject(error));
+
+api.interceptors.response.use((response) => response, async (error) => {
+  const originalRequest = error.config;
+  if (error.response.status === 401 && !originalRequest._retry) {
+    originalRequest._retry = true;
+    try {
+      const refresh = localStorage.getItem("refresh");
+      const response = await axios.post(`${API_BASE_URL}/refresh-token/`, {refresh});
+      const {access} = response.data;
+      localStorage.setItem("access", access);
+      api.defaults.headers.common["Authorization"] = `Bearer ${access}`;
+      return api(originalRequest);
+    } catch (refreshError) {
+      console.error("Token refresh failed:", refreshError);
+    }
+  }
+  return Promise.reject(error);
+});
 
 export default api;
